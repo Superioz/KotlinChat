f59abefd6a1eff801018f393e444453b69391808
Tobias B <germansuperioz@yahoo.de>
2019-03-22 15:01:38 +0100

    Added chat functionality

--- src/main/java/de/superioz/kotlinchat/client/Client.kt
+++ src/main/java/de/superioz/kotlinchat/client/Client.kt
@@ -1,9 +1,52 @@
 package de.superioz.kotlinchat.client
 
+import java.io.BufferedOutputStream
+import java.io.InputStreamReader
+import java.net.Socket
+import java.net.SocketException
+import java.nio.charset.Charset
+import java.util.*
+
 
 /**
  * @author Tobias Büser
  */
 fun main(args: Array<String>) {
     // start client and connect to hardcoded server
+    val socket = Socket("localhost", 13378)
+
+    println("Connected to server.")
+
+    Thread {
+        while(true) {
+            try {
+                val streamReader = InputStreamReader(socket.getInputStream())
+
+                streamReader.forEachLine {
+                    println(it)
+                }
+            } catch (ex: SocketException) {
+                println("Disconnected from server")
+                System.exit(0)
+            }
+        }
+    }.start()
+
+    println("Waiting for input ..")
+
+    val consoleReader = Scanner(System.`in`)
+    while(consoleReader.hasNext()) {
+        val message = consoleReader.nextLine()
+
+        println(message)
+        try {
+            val output = BufferedOutputStream(socket.getOutputStream())
+            output.write((message + System.lineSeparator()).toByteArray(Charset.forName("UTF-8")))
+            output.flush()
+        } catch(ex: SocketException) {
+            println("Disconnected from server.")
+            System.exit(0)
+        }
+    }
+
 }
--- src/main/java/de/superioz/kotlinchat/server/Server.kt
+++ src/main/java/de/superioz/kotlinchat/server/Server.kt
@@ -1,9 +1,50 @@
 package de.superioz.kotlinchat.server
 
+import java.io.InputStreamReader
+import java.net.ServerSocket
+import java.net.Socket
+import java.net.SocketException
+import java.nio.charset.Charset
+
 
 /**
  * @author Tobias Büser
  */
 fun main(args: Array<String>) {
     // startup server, thats basically it
+
+    val server = ServerSocket(13378)
+    println("Server socket bound to localhost:${server.localPort}")
+
+    val clients = mutableListOf<Socket>()
+
+    while(true) {
+        println("Waiting for connection ..")
+        val socket = server.accept()
+        println("Client connected. (${socket.inetAddress.hostAddress})")
+
+        clients.add(socket)
+
+        Thread {
+            try {
+                val streamReader = InputStreamReader(socket.getInputStream())
+
+                streamReader.forEachLine {
+                    val id = clients.indexOf(socket)
+                    val message = "$id says: $it"
+
+                    println(message)
+
+                    clients.forEach {s ->
+                        s.getOutputStream().write((message + "\n").toByteArray(Charset.forName("UTF-8")))
+                        s.getOutputStream().flush()
+                    }
+                }
+            } catch (ex: SocketException) {
+                println("Client disconnected")
+
+                clients.remove(socket)
+            }
+        }.start()
+    }
 }
