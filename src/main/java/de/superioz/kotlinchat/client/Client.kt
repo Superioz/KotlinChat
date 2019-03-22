74e075c4505db793c14a50e7f960b3307590aae6
Tobias B <germansuperioz@yahoo.de>
2019-03-22 12:49:16 +0100

    Created main files

---
+++ src/main/java/de/superioz/kotlinchat/client/Client.kt
@@ -0,0 +1,9 @@
+package de.superioz.kotlinchat.client
+
+
+/**
+ * @author Tobias Büser
+ */
+fun main(args: Array<String>) {
+    // start client and connect to hardcoded server
+}
---
+++ src/main/java/de/superioz/kotlinchat/server/Server.kt
@@ -0,0 +1,9 @@
+package de.superioz.kotlinchat.server
+
+
+/**
+ * @author Tobias Büser
+ */
+fun main(args: Array<String>) {
+    // startup server, thats basically it
+}
